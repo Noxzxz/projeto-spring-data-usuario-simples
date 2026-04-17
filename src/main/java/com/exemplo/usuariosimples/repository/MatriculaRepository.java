ce26592bee1f6eb24f103d38ea4c107eb578863c
Noxzxz <ihann664@gmail.com>
2026-04-17 12:30:30 -0300

    Correção de erros
    Correção na matricula e install do pom

--- src/main/java/com/exemplo/usuariosimples/repository/MatriculaRepository.java
+++ src/main/java/com/exemplo/usuariosimples/repository/MatriculaRepository.java
@@ -1,7 +1,8 @@
 package com.exemplo.usuariosimples.repository;
 
+import com.exemplo.usuariosimples.domain.Matricula;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.exemplo.usuariosimples.domain.matriculaEntity;
+import com.exemplo.usuariosimples.domain.Matricula;
 
-public interface MatriculaRepository extends JpaRepository<matriculaEntity, Long> {
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
 }
