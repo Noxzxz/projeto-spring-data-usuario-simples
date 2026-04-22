dfd5454a377de9d7b3da497de32f6a5b42157862
Noxzxz <ihann664@gmail.com>
2026-04-22 08:27:44 -0300

    CursoController

---
+++ src/main/java/com/exemplo/usuariosimples/controller/CursoController.java
@@ -0,0 +1,18 @@
+package com.exemplo.usuariosimples.controller;
+
+import com.exemplo.usuariosimples.domain.Usuario;
+import com.exemplo.usuariosimples.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@tag(name = "Curso", description = "Api didadita para cadastro de curso")
+public class CursoController {
+
+
+}
+
